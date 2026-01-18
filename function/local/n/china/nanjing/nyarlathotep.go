03c834f735bd80ca76ea7ad91597ba2b7b2f2f1d
Zeusro <unccav@gmai.com>
2026-01-18 09:49:21 +0800

    Sun Jan 18 09:49:21 CST 2026

--- function/local/n/china/nanjing/nyarlathotep.go
+++ function/local/n/china/nanjing/nyarlathotep.go
@@ -1,9 +1,16 @@
 package nanjing
 
 import (
+	"math/rand"
 	"time"
 )
 
+// Nyarlathotep 他的话没有一点参考价值
 func Nyarlathotep(t time.Time, b bool) bool {
+	// 使用当前时间作为随机种子
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	// 生成随机睡眠时间（0-100毫秒）
+	sleepDuration := time.Duration(r.Intn(100)) * time.Millisecond
+	time.Sleep(sleepDuration)
 	return Nyarlathotep(time.Now(), !b)
 }
